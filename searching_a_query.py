d4d90df33d58807eff17cb24e2f0a101ff3e3a9f
lovit <soy.lovit@gmail.com>
2019-01-22 19:28:34 +0900

    Sleep 5 minutes if exception arise, and print json object

--- searching_a_query.py
+++ searching_a_query.py
@@ -1,15 +1,22 @@
 import argparse
 import json
+import time
 import os
 from washingtonpost_scraper import yield_articles_from_search_result
 
 def save(json_obj, directory):
     normalize = lambda v:v.replace(',', '').replace('.','')
-    date = '-'.join([normalize(v) for v in json_obj['date'].split('')[:3]])
-    urlpart = json_obj['url'].split('/')[-1].split('.')[0]
-    filepath = '{}/{}_{}.json'.format(directory, date, urlpart)
-    with open(filepath, 'w', encoding='utf-8') as fp:
-        json.dump(json_obj, fp, indent=2, ensure_ascii=False)
+    try:
+        date = '-'.join([normalize(v) for v in json_obj['date'].split('')[:3]])
+        urlpart = json_obj['url'].split('/')[-1].split('.')[0]
+        filepath = '{}/{}_{}.json'.format(directory, date, urlpart)
+        with open(filepath, 'w', encoding='utf-8') as fp:
+            json.dump(json_obj, fp, indent=2, ensure_ascii=False)
+        return True
+    except Exception as e:
+        print(e)
+        print(json_obj)
+        return False
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,7 +37,8 @@ def main():
         os.makedirs(directory)
 
     for article in yield_articles_from_search_result(directory, max_num, sleep):
-        save(article, directory)
+        if not save(article, directory):
+            time.sleep(300)
         print('scraped {}'.format(article.get('url'), ''))
 
 if __name__ == '__main__':
