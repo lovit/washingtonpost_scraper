148d8eda6a7380baf27995e5a28b405a93184284
lovit <soy.lovit@gmail.com>
2019-03-08 23:33:22 +0900

    Re-implement searching_a_query

--- searching_a_query.py
+++ searching_a_query.py
@@ -3,53 +3,67 @@ import json
 import time
 import os
 import re
-from washingtonpost_scraper import yield_articles_from_search_result
+from washingtonpost_scraper import get_urls_from_a_search_page
+from washingtonpost_scraper import parse_page
 
-def save(json_obj, directory):
-
-    datepattern = re.compile('\d{4}-\d{2}-\d{2}')
-    normalize = lambda v:v.replace(',', '').replace('.','')
-    try:
-        date_strf = json_obj.get('date_strf', '')
-        if not datepattern.match(date_strf):
-            return False
-        urlpart = json_obj['url'].split('/')[-1].split('.')[0]
-        filepath = '{}/{}_{}.json'.format(directory, date_strf, urlpart)
-        with open(filepath, 'w', encoding='utf-8') as fp:
-            json.dump(json_obj, fp, indent=2, ensure_ascii=False)
-        return True
-    except Exception as e:
-        print(e)
-        print('url : {}'.format(json_obj.get('url', 'Not found')))
-        return False
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory', type=str, default='./output', help='Output directory')
-    parser.add_argument('--sleep', type=float, default=10, help='Sleep time for each submission (post)')
-    parser.add_argument('--max_num', type=int, default=10, help='Number of scrapped articles')
+    parser.add_argument('--sleep', type=float, default=3, help='Sleep time for each submission (post)')
+    parser.add_argument('--begin_num', type=int, default=0, help='Number of scrapped articles')
+    parser.add_argument('--max_num', type=int, default=10000, help='Number of scrapped articles')
     parser.add_argument('--query', type=str, default='korea', help='Number of scrapped articles')
+    parser.add_argument('--force', dest='force', action='store_true')
+
 
     args = parser.parse_args()
     directory = args.directory
     sleep = args.sleep
+    begin_num = args.begin_num
     max_num = args.max_num
+    max_num = max(max_num, 20)
     query = args.query
+    force = args.force
 
     # check output directory
     directory += '/%s' % query
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for article in yield_articles_from_search_result(directory, max_num, sleep):
-        if not save(article, directory):
-            print('Sleep 5 minutes because exception occurs')
-            time.sleep(300)
-        date_strf = article.get('date_strf', '')
-        url = article.get('url', '')
-        if date_strf:
-            url = '  ' + url
-        print('scraped {}{}'.format(date_strf, url))
+    stop = False
+    for startat in range(begin_num, max_num, 20):
+
+        # startat loop
+        if stop:
+            break
+        print('startat = {}'.format(startat))
+
+        # scrap article urls
+        urls = get_urls_from_a_search_page(query, startat)
+
+        for url in urls:
+
+            # scrap
+            json_obj = parse_page(url)
+            date_strf = json_obj['date_strf']
+            last_part = url.split('/')[-1].split('.')[0]
+            filepath = '{}/{}_{}.json'.format(directory, date_strf, last_part)
+
+            # check file has been already scraped
+            if os.path.exists(filepath):
+                if not force:
+                    stop = True
+                    break
+                print('Already scraped from {}'.format(url))
+
+            # save
+            with open(filepath, 'w', encoding='utf-8') as f:
+                json.dump(json_obj, f, ensure_ascii=False, indent=2)
+
+            print('url = {}'.format(url))
+            print('file path = {}'.format(filepath), end='\n\n')
+            time.sleep(sleep)
 
 if __name__ == '__main__':
     main()
