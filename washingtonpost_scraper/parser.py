38cfe9bdd2fdcea8bce2c5f4e965f6b0312a4413
lovit <soy.lovit@gmail.com>
2019-03-11 02:33:31 +0900

    Available to scrap archived articles (url = /AR...)

--- washingtonpost_scraper/parser.py
+++ washingtonpost_scraper/parser.py
@@ -1,4 +1,10 @@
+import re
 from .utils import get_soup
+
+def parse_page(url):
+    if '/AR' in url:
+        return parse_page_ar(url)
+    return parse_page_basic(url)
 
 def parse_content(soup):
     phrases = [p.text.strip() for p in soup.select('article[itemprop=articleBody] p')]
@@ -30,7 +36,7 @@ def parse_category(soup):
         return ''
     return a[0].text.strip()
 
-def parse_page(url):
+def parse_page_basic(url):
     soup = get_soup(url)
     if soup is None:
         return {}
@@ -45,4 +51,59 @@ def parse_page(url):
     }
     date_strf = '-'.join(url.split('/')[-4:-1])
     json_obj['date_strf'] = date_strf
+    return json_obj
+
+def parse_author_date_ar(soup):
+    ad = soup.select('div[id=article] font[size=2]')
+    if not ad:
+        return '', ''
+    byline = soup.select('div[id=byline]')
+    if byline:
+        author_ = byline[0].text.strip()
+        if author_.split()[0].lower() == 'by':
+            author = ' '.join(author_.split()[1:])
+        else:
+            author = author_
+    else:
+        author_ = ''
+        author = ''
+
+    if author_:
+        date = ad[0].text.replace(author_, '').strip()
+    else:
+        date = ''
+
+    return author, date
+
+def parse_date_strf_from_url(url):
+    datepattern = re.compile('\d{4}/\d{2}/\d{2}')
+    date_strf = datepattern.findall(url)
+    if date_strf:
+        return date_strf[0].replace('/', '-')
+    return ''
+
+def parse_head_ar(soup):
+    headline = soup.select('h1')
+    if not headline:
+        return ''
+    return headline[0].text.strip()
+
+def parse_content_ar(soup):
+    content = [p.text.strip() for p in soup.select('div[id=article_body] p')]
+    if not content:
+        return ''
+    return '\n'.join(content)
+
+def parse_page_ar(url):
+    soup = get_soup(url)
+    author, date = parse_author_date_ar(soup)
+    json_obj = {
+        'url': url,
+        'content': parse_content_ar(soup),
+        'author': author,
+        'date': date,
+        'headline': parse_head_ar(soup),
+        'category': '',
+        'date_strf': parse_date_strf_from_url(url)
+    }
     return json_obj
